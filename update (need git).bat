19f4beeaec22c7f52a30eccad24ac57b2d476ea3
p2-3r <159620178+p2-3r@users.noreply.github.com>
2024-02-11 11:02:38 +0900

    Add files via upload

--- update (need git).bat
+++ update (need git).bat
@@ -4,9 +4,17 @@ echo running update...
 
 git clone https://github.com/p2-3r/Discord-ReadTextBot-for-Style-Bert-VITS2-API
 
-cd Discord-ReadTextBot-for-Style-Bert-VITS2-API
-rd /S /Q .git
+if exist "Discord-ReadTextBot-for-Style-Bert-VITS2-API" (
 
-cd ..
+    cd Discord-ReadTextBot-for-Style-Bert-VITS2-API
+
+    if exist ".git" (
+
+        rd /S /Q .git
+
+    )
+
+    cd ..
+)
 
 python update.py
